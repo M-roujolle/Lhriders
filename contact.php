8adb4c09dad9d6b6e41d53ba124d5f6cc9410a5d
Maxime <moostafarai@hotmail.fr>
2022-01-02 15:49:48 +0100

    correction bug marging formulaire contact

--- contact.php
+++ contact.php
@@ -67,109 +67,100 @@ require("controllers/controllercontact.php");
                     <a class="btn btn-success mt-5" href="index.php">Retour à l'accueil</a>
         </div>
 
-
-
     <?php } else { ?>
-        <div class="row justify-content-center">
-            <div class=" col-lg-6 mt-5">
-                <div class="navColor text-white">
-                    <h1 class="text-center pt-5 mb-5">Formulaire de Contact</h1>
 
+        <div class="row m-0">
+            <div class="d-flex justify-content-center">
+                <div class=" col-lg-6 mt-5">
+                    <div class="navColor text-white d-flex justify-content-center">
+                        <form action="contact.php" method="POST" class="ps-3 pe-3">
+                            <h1 class="text-center pt-3 mb-3">Formulaire de Contact</h1>
 
-                    <form action="contact.php" method="POST" class="ps-3 pe-3">
+                            <div class="mb-3">
+                                <label for="nom" class="form-label">Nom : </label><span class="text-danger">
+                                    <?=
+                                    $arrayError["nom"] ?? " ";
+                                    ?>
+                                </span>
+                                <input value="<?= isset($_POST["nom"]) ? htmlspecialchars($_POST["nom"]) : "" ?>" name="nom" type="text" class="form-control w-75" id="nom" placeholder="Ex : Dupont...">
 
-                        <div class="mb-3">
-                            <label for="nom" class="form-label">Nom : </label><span class="text-danger">
-                                <?=
-                                $arrayError["nom"] ?? " ";
-                                ?>
-                            </span>
-                            <input value="<?= isset($_POST["nom"]) ? htmlspecialchars($_POST["nom"]) : "" ?>" name="nom" type="text" class="form-control w-50" id="nom" placeholder="Ex : Dupont...">
+                                <label for="prenom" class="form-label mt-1">Prénom : </label><span class="text-danger">
+                                    <?=
+                                    $arrayError["prenom"] ?? " ";
+                                    ?>
+                                </span>
+                                <input value="<?= isset($_POST["prenom"]) ? htmlspecialchars($_POST["prenom"]) : "" ?>" name=" prenom" type="text" class="form-control w-75" id="prenom" placeholder="Ex : Jean...">
 
-                            <label for="prenom" class="form-label mt-1">Prénom : </label><span class="text-danger">
-                                <?=
-                                $arrayError["prenom"] ?? " ";
-                                ?>
-                            </span>
-                            <input value="<?= isset($_POST["prenom"]) ? htmlspecialchars($_POST["prenom"]) : "" ?>" name=" prenom" type="text" class="form-control w-50" id="prenom" placeholder="Ex : Jean...">
-
-                            <label for="mail" class="form-label mt-1">Mail : </label><span class="text-danger">
-                                <?=
-                                $arrayError["mail"] ?? " ";
-                                ?>
-                            </span>
-                            <input value="<?= isset($_POST["mail"]) ? htmlspecialchars($_POST["mail"]) : "" ?>" name=" mail" type="mail" class="form-control w-50" id="mail" placeholder="Ex : monmail@mail.fr...">
-
+                                <label for="mail" class="form-label mt-1">Mail : </label><span class="text-danger">
+                                    <?=
+                                    $arrayError["mail"] ?? " ";
+                                    ?>
+                                </span>
+                                <input value="<?= isset($_POST["mail"]) ? htmlspecialchars($_POST["mail"]) : "" ?>" name=" mail" type="mail" class="form-control w-75" id="mail" placeholder="Ex : monmail@mail.fr...">
+                            </div>
                             <div class="mt-1">
                                 <label for="inputPassword6" class="col-form-label">Sujet :</label><span class="text-danger">
                                     <?=
                                     $arrayError["select"] ?? "";
                                     ?>
                                 </span>
-                                <select for="select" name="select" required class="form-select w-50">
+                                <select for="select" name="select" required class="form-select w-75">
                                     <option selected value="0">Selectionner votre sujet</option>
                                     <option value="1">Un problème sur le site ?</option>
                                     <option value="2">Besoin d'informations ?</option>
                                     <option value="3">Autres</option>
                                 </select>
-
-                                <div class="mt-1">
-                                    <p>Veuillez décrire votre demande :</p>
-                                    <label for="inputPassword6" class="col-form-label"></label><span class="text-danger">
-                                        <?=
-                                        $arrayError["story"] ?? "";
-                                        ?>
-                                    </span>
-
-                                    <textarea id="story" name="story" required rows="10" cols="45"><?= isset($_POST["story"]) ? htmlspecialchars($_POST["story"]) : "" ?></textarea>
-                                </div>
-                                <div class="mb-3 form-check ms-1">
-                                    <input type="checkbox" class="form-check-input" name="checkBox" id="checkBox">
-                                    <label class="form-check-label" for="checkBox">Accepter les CGU</label><span class="text-danger">
-                                        <?=
-                                        $arrayError["checkBox"] ?? " ";
-                                        ?>
-                                    </span>
-                                </div>
-                                <button type="submit" class="btn btn-primary mb-5 ms-1">Envoyer</button>
                             </div>
-                        </div>
+                            <div class="mt-1">
+                                <p>Veuillez décrire votre demande :</p>
+                                <label for="inputPassword6" class="col-form-label"></label><span class="text-danger">
+                                    <?=
+                                    $arrayError["story"] ?? "";
+                                    ?>
+                                </span>
 
-                    </form>
-
+                                <textarea id="story" name="story" required rows="10" cols="38"><?= isset($_POST["story"]) ? htmlspecialchars($_POST["story"]) : "" ?></textarea>
+                            </div>
+                            <div class="mb-3 form-check ms-1">
+                                <input type="checkbox" class="form-check-input" name="checkBox" id="checkBox">
+                                <label class="form-check-label" for="checkBox">Accepter les CGU</label><span class="text-danger">
+                                    <?=
+                                    $arrayError["checkBox"] ?? " ";
+                                    ?>
+                                </span>
+                            </div>
+                            <button type="submit" class="btn btn-primary mb-5 ms-1">Envoyer</button>
+                        </form>
+                    </div>
                 </div>
             </div>
-        <?php } ?>
+        </div>
+    <?php } ?>
 
+    <!-- FOOTER----------------------------------------------------------------------------------------------------------------------------------------->
+    <footer>
+        <div class="footer-basic mt-5 text-center selectColor pb-5 pt-5">
 
+            <ul class="list-inline">
+                <li class="list-inline-item">
+                    <a class="nav-link active text-white" aria-current="page" href="contact.php">Contact</a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="nav-link active text-white" aria-current="page" href="cgu.php">Conditions génerales d'utilisation</a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="nav-link active text-white" aria-current="page" href="mentionslegales.php">Mentions Légales</a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="nav-link active text-white" aria-current="page" href="cookies.php">Cookies</a>
+                </li>
+            </ul>
+            <p class="copyright">©Moto Poto 2022</p>
+        </div>
+    </footer>
 
-        <!-- FOOTER----------------------------------------------------------------------------------------------------------------------------------------->
-
-        <footer>
-            <div class="footer-basic mt-5 text-center selectColor pb-5 pt-5">
-
-                <ul class="list-inline">
-                    <li class="list-inline-item">
-                        <a class="nav-link active text-white" aria-current="page" href="contact.php">Contact</a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a class="nav-link active text-white" aria-current="page" href="cgu.php">Conditions génerales d'utilisation</a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a class="nav-link active text-white" aria-current="page" href="mentionslegales.php">Mentions Légales</a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a class="nav-link active text-white" aria-current="page" href="cookies.php">Cookies</a>
-                    </li>
-                </ul>
-                <p class="copyright">©Moto Poto 2022</p>
-            </div>
-        </footer>
-
-
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-        </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
